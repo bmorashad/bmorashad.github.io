d6569c7e6cfa78192e715dbe74c1fe691d8023dd
Mohamed Rashad <bmorashad@gmail.com>
2022-08-24 21:11:40 +0530

    Migrated to svelte from sveltekit

--- bmorashad.github.io/src/App.svelte
+++ bmorashad.github.io/src/App.svelte
@@ -24,6 +24,13 @@
 	}); */
 </script>
 
+<svelte:head>
+	<link
+		href="https://fonts.googleapis.com/css2?family=Lexend+Deca:wght@100;200;300;400;500;600;700;800;900&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 <div class="nav-container">
 	<nav>
 		<a href="#skills" class="menu-link">Skills</a>
